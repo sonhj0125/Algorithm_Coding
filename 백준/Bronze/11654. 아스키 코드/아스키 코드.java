d10163e6e1714db69508a8d2c909d4bdc82956b8
Hyejeong Son <154120089+sonhj0125@users.noreply.github.com>
2024-10-05 22:00:37 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+/*    Scanner sc = new Scanner(System.in);
+
+    int c = sc.next().charAt(0);
+
+    System.out.print(c);*/
+
+    int a = System.in.read();
+    System.out.print(a);
+
+  }
+
+}
