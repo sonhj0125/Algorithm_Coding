699e720ba9df6815cda124683241acafdd575f56
Hyejeong Son <154120089+sonhj0125@users.noreply.github.com>
2024-10-05 21:56:03 +0900

    [Bronze V] Title: A-B, Time: 176 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    
+    int A = scanner.nextInt();
+    int B = scanner.nextInt();
+
+    System.out.print(A-B);
+
+  }
+
+}
