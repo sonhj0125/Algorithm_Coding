4bfe27ba82364d653bbfe751984a85330233096a
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2025-01-07 16:26:23 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.11 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        /*
+            String answer = "";
+
+            for(int i=my_string.length()-1; i>=0; i--) {
+                answer += my_string.charAt(i);
+            }
+
+            return answer;
+        */
+        StringBuffer str = new StringBuffer(my_string);
+        
+        return str.reverse().toString();
+        
+    }
+}
