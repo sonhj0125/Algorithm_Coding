c163f254cf09685e78b47a6b07d25d8e2990613d
손혜정 <154120089+sonhj0125@users.noreply.github.com>
2025-01-08 19:27:29 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -2,9 +2,7 @@ class Solution {
     public int solution(String message) {
         int answer = 0;
         
-        for(int i=0; i<message.length(); i++){
-            answer = 2 * message.length();    
-        }
+        answer = 2 * message.length();
         
         return answer;
     }
